c7ffed007e4869d494f694366df20e5f2c856184
Ismaël Dabo <ismael.dabo@hetic.net>
2018-02-15 14:52:01 +0100

    infos sup

--- phpUpdate 2/doadd.php
+++ phpUpdate 2/doadd.php
@@ -9,13 +9,15 @@ if (!isset($_POST['title'])) {
 //Connexion
 require_once "connect.php";
 
-// Requête SQL visant les colonnes et les valeurs à changer
+// Requête SQL trigg. -> adresse colonnes et valeurs à changer
 
 $requete = "INSERT INTO `Article` (`title`, `description`, `price`, `image`) VALUES (:title, :description, :price, :image);";
 
 $stmt = $conn->prepare($requete);
 
 // Envoi des données du formulaire récupérées via $_POST
+
+// $stmt = sécurité injec°
 
 $stmt->bindValue(':title', $_POST['title']);
 $stmt->bindValue(':description', $_POST['description']);
