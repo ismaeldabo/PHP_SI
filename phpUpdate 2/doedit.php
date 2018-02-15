46bb5b4959812aa70f5a9d2e0a2a46193ad3fcc4
LeKawa <hector.travaille@hetic.net>
2018-02-15 18:28:42 +0100

    finipourYan

--- phpUpdate 2/doedit.php
+++ phpUpdate 2/doedit.php
@@ -11,11 +11,16 @@ SET
 WHERE
    id = :id
 ;";
+/** @var $stmt */
 $stmt = $conn->prepare($sql);
+//on réattribue toutes les valeurs avec le bind value
 $stmt->bindValue(':title', $_POST['title']);
 $stmt->bindValue(':description', $_POST['description']);
 $stmt->bindValue(':price', $_POST['price']);
 $stmt->bindValue(':image', $_POST['image']);
 $stmt->execute();
+// A la fin on l'éxecute
+
 header('Location: ../index.php?id='.$conn->lastInsertId());
+// nous revenons vers index.php avec le projet que nous avons éditer
 
