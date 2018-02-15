40a9772424f474f721e6eb9d8b219bd5ffc19366
LeKawa <hector.travaille@hetic.net>
2018-02-15 18:57:39 +0100

    fini

--- phpUpdate 2/home.php
+++ phpUpdate 2/home.php
@@ -6,15 +6,14 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/styleHome.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Home</title>
 </head>
 <body>
 
-  <h1 class="addProject">Home in Future.
-    <form action="index.php?id=1" class="addBtn">
-        <input type="submit" name="submit" href="#">
-    </form>
-  </h1>
+  <h1 class="addProject">Objects From Future. </h1>
+
+  <a href="index.php?id=1" class="link">CONTINUE</a>
 
 </body>
 </html>
