d25424071c27c67ba72887a6f88ee4d3fb0f9d9e
Ismaël Dabo <ismael.dabo@hetic.net>
2018-02-15 13:54:51 +0100

    delete

--- phpUpdate 2/doadd.php
+++ phpUpdate 2/doadd.php
@@ -1,6 +1,7 @@
 <?php
 
 // Verification du contenu du titre, si vide : redirection.
+
 if (!isset($_POST['title'])) {
     header('Location: index.php?nopostdata');
     exit;
@@ -9,11 +10,13 @@ if (!isset($_POST['title'])) {
 require_once "connect.php";
 
 // Requête SQL visant les colonnes et les valeurs à changer
+
 $requete = "INSERT INTO `Article` (`title`, `description`, `price`, `image`) VALUES (:title, :description, :price, :image);";
 
 $stmt = $conn->prepare($requete);
 
 // Envoi des données du formulaire récupérées via $_POST
+
 $stmt->bindValue(':title', $_POST['title']);
 $stmt->bindValue(':description', $_POST['description']);
 $stmt->bindValue(':price', $_POST['price']);
@@ -22,4 +25,5 @@ $stmt->bindValue(':image', $_POST['image']);
 $stmt->execute();
 
 // redirection à l'index 'lastInsertId()'
+
 header("location:index.php?id=".$conn->lastInsertId());
--- phpUpdate 2/dodelete.php
+++ phpUpdate 2/dodelete.php
@@ -1,18 +1,21 @@
 <?php
 
+// Connexion au serveur SQL
 require_once "connect.php";
+
+// Envoi de la requête de suppression au serveur SQL avec l'ID
 $requete = "DELETE FROM 
 `article` 
 WHERE 
 
 id = :id;";
+
+// Envoi de l'ID a supprimer au serveur
 $stmt = $conn->prepare($requete);
 $stmt->bindValue(':id', $_POST['id']);
 $stmt->execute();
 
-
+// Redirection Home
 header('Location: ../home.php');
 
 ?>
-
-
