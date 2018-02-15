ef3b53e6f0a538af566f805ccaba5bcf230569b0
Ismaël Dabo <ismael.dabo@hetic.net>
2018-02-15 11:14:34 +0100

    Commentaires doAdd

--- phpUpdate 2/connect.php
+++ phpUpdate 2/connect.php
@@ -1,7 +1,9 @@
 <?php
-$username = 'root';
-$password = 'root';
+//$username = 'root';
+//$password = 'root';
 
+
+// Connexion via PDO
 try {
     $conn = new PDO('mysql:host=localhost;dbname=Data','root', 'root');
 } catch (PDOException $exception) {
--- phpUpdate 2/doadd.php
+++ phpUpdate 2/doadd.php
@@ -1,15 +1,19 @@
 <?php
+
+// Verification du contenu du titre, si vide : redirection.
 if (!isset($_POST['title'])) {
     header('Location: index.php?nopostdata');
     exit;
 }
-
+//Connexion
 require_once "connect.php";
 
+// Requête SQL visant les colonnes et les valeurs à changer
 $requete = "INSERT INTO `Article` (`title`, `description`, `price`, `image`) VALUES (:title, :description, :price, :image);";
 
 $stmt = $conn->prepare($requete);
 
+// Envoi des données du formulaire récupérées via $_POST
 $stmt->bindValue(':title', $_POST['title']);
 $stmt->bindValue(':description', $_POST['description']);
 $stmt->bindValue(':price', $_POST['price']);
@@ -17,4 +21,5 @@ $stmt->bindValue(':image', $_POST['image']);
 
 $stmt->execute();
 
+// redirection à l'index 'lastInsertId()'
 header("location:index.php?id=".$conn->lastInsertId());
